ff25466375533ce1dd7830add486120b6b478283
trevorelvey <trevor.elvey@gmail.com>
2016-03-18 11:55:48 -0700

    functioning lookup and display

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,7 +1,31 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+var apiKey = require('./../.env').apiKey;
+
+// exports.getRepos = function(){
+//
+//   $.get('https://api.github.com/users/' + $username + '?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
+
+$(document).ready(function() {
+  $('#getRepos').click(function() {
+    var user = $('#username').val();
+    $('#username').val("");
+    $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response) {
+      $('.showUsername').text(response.login);
+      $('.showRepoCount').text("Public Repos: " + response.public_repos);
+    });
+    $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response) {
+      // $('.showRepos').text(response.all.name);
+      for(var i = 0; i < response.length; i++) {
+        $(".showRepos").prepend('<ul><li>' + '<a href="https://github.com/' + (response[i].full_name) + '">' + (response[i].name) + '</a>' + ' ' + (response[i].description) + '</li></ul>');
+
+      }
+
+    }).fail(function(error) {
+      $('.showRepos').text(error.responseJSON.message);
+    });
   });
-};
+});
